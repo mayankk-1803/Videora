d79031d8582fdfc428b9690a9a8c4c3ca81ed3aa
mayankk-1804 <mayankmathur1804@gmail.com>
2026-02-04 20:07:47 +0530

    DB CONNECTED

---
+++ lib/db.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI!
+
+if(!MONGODB_URI){
+    throw new Error("Please define the MONGODB_URI environment variable inside .env")
+}
+
+let cached = global.mongoose
+
+if(!cached){
+    cached = global.mongoose = {conn: null, promise: null};
+}
+
+export async function connectToDatabase() {
+    if(cached.conn){
+        return cached.conn;
+    }
+
+    if(!cached.promise){
+        mongoose.connect(MONGODB_URI)
+        .then(() => mongoose.connection)
+    }
+
+    try {
+        cached.conn = await cached.promise;
+    } catch (error) {
+        cached.promise = null;
+    }
+    return cached.conn;
+}
